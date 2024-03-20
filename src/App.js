1b6f1f98d313ee60249e8618320244c21def8586
marcusmcb <marcusmcb@gmail.com>
2024-03-20 11:36:43 -0700

    finished responsive styling on mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Fragment } from 'react'
 
 import './App.css'
-import '@fontsource-variable/outfit';
+import '@fontsource-variable/outfit'
 import '@fontsource/outfit/300.css'
 import '@fontsource-variable/big-shoulders-display'
 import '@fontsource/big-shoulders-display/800.css'
-import Nav from './components/Nav'
+
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContentPrimary from './components/MainContentPrimary'
@@ -14,7 +14,6 @@ import MainContentSecondary from './components/MainContentSecondary'
 const App = () => {
 	return (
 		<Fragment>
-			{/* <Nav /> */}
 			<Header />
 			<MainContentPrimary />
 			<MainContentSecondary />
